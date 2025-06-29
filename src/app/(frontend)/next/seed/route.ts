aa9dcf6ceb6caf944117e68b478a7783670f5951
Richard Kovacs <richardkovacs.it@gmail.com>
2025-06-29 20:47:50 +0200

    style: fix type error

--- src/app/(frontend)/next/seed/route.ts
+++ src/app/(frontend)/next/seed/route.ts
@@ -1,4 +1,4 @@
-import { createLocalReq, getPayload } from 'payload'
+import { createLocalReq, getPayload, User } from 'payload'
 import { seed } from '@/endpoints/seed'
 import config from '@payload-config'
 import { headers } from 'next/headers'
@@ -19,7 +19,8 @@ export async function POST(): Promise<Response> {
   try {
     // Create a Payload request object to pass to the Local API for transactions
     // At this point you should pass in a user, locale, and any other context you need for the Local API
-    const payloadReq = await createLocalReq({ user }, payload)
+    // TODO: Fix this ignored type error
+    const payloadReq = await createLocalReq({ user: user as User }, payload)
 
     await seed({ payload, req: payloadReq })
 
