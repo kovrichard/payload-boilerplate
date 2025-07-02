2b26c501f7f9da98814385fd235ac41d4ca00d25
Mate Kiss <kissmate.work@gmail.com>
2025-07-02 20:40:26 +0200

    build: install and config million lint

--- next.config.js
+++ next.config.js
@@ -1,36 +1,42 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+import MillionLint from "@million/lint";
+import { withPayload } from "@payloadcms/next/withPayload";
 
-import redirects from './redirects.js'
+import redirects from "./redirects.js";
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || "http://localhost:3000";
+
+// Enable Million Lint based on environment variable
+const enableMillionLint = process.env.MILLION_LINT === "true";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
+        const url = new URL(item);
 
         return {
           hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
+          protocol: url.protocol.replace(":", ""),
+        };
       }),
     ],
   },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
-      '.cjs': ['.cts', '.cjs'],
-      '.js': ['.ts', '.tsx', '.js', '.jsx'],
-      '.mjs': ['.mts', '.mjs'],
-    }
+      ".cjs": [".cts", ".cjs"],
+      ".js": [".ts", ".tsx", ".js", ".jsx"],
+      ".mjs": [".mts", ".mjs"],
+    };
 
-    return webpackConfig
+    return webpackConfig;
   },
   reactStrictMode: true,
   redirects,
-}
+};
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default enableMillionLint
+  ? MillionLint.next(withPayload(nextConfig, { devBundleServerPackages: false }))
+  : withPayload(nextConfig, { devBundleServerPackages: false });
